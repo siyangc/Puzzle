e1bd15676513f8b81c0e03183580c4709bbdbf96
siyangc <csy602994381@gmail.com>
2020-06-19 16:39:06 +1000

    'add'

---
+++ build/precache-manifest.e3c548c773d7d53aeee798b87e158944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9756421f50f54d868912ac87b770f0e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "105200980c8231499bda",
+    "url": "/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "860ed0b10bed3f0cf82e",
+    "url": "/static/js/2.24a15bcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24a15bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105200980c8231499bda",
+    "url": "/static/js/main.0576e225.chunk.js"
+  },
+  {
+    "revision": "716256ba1f9c240d97cb",
+    "url": "/static/js/runtime-main.9431f247.js"
+  }
+]);
